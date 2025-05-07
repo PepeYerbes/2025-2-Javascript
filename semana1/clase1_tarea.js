d2debd45cd668a4d1bb5329ef979ead720aabc89
PepeYerbes <ppyerbes@gmail.com>
2025-05-06 20:23:56 -0600

    aorendi

--- semana1/clase1_tarea.js
+++ semana1/clase1_tarea.js
@@ -7,9 +7,19 @@ async function main() {
   const num2 = Number(await ask('Ingresa el segundo número: '));
 
   const suma = num1 + num2;
+  const resta = num1 - num2;
+    const multiplicacion = suma * resta;
+    const division = suma / resta;
+    const promedio = (num1 + num2) / 2;
+
+
   // TODO: Implementar la resta, multiplicación y división
 
   console.log('Suma:', suma);
+  console.log('Resta:', resta);
+    console.log('Multiplicación:', multiplicacion);
+    console.log('División:', division);
+    console.log('Promedio:', promedio);
   // TODO: Mostrar el resultado de la resta, multiplicación y división
 
 }
