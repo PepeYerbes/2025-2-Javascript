eac6819e7bd93f5c4b54f9c61777562a9e07c40a
PepeYerbes <ppyerbes@gmail.com>
2025-05-18 23:42:11 -0600

    Agrega ejercicio4.js con lógica para filtrar nombres que inician con vocal

---
+++ semana2/ejercicio4.js
@@ -0,0 +1,18 @@
+const { ask } = require('../helpers/input');
+
+async function main() {
+  const nombres = ["Almita", "Luz", "Michell", "Jessica", "Erick", "Esly", "Oscar", "Pepe","Ilse","Alexa","Leonardo","Noemi","Fernanda","Pablo","Uriel"];
+  const vocales = ["a", "e", "i", "o", "u", "á", "é", "í", "ó", "ú"];
+  const result = [];
+
+  for (let index1 = 0; index1 < nombres.length; index1++) {
+    const primerLetra = nombres[index1][0].toLowerCase();
+    if (vocales.includes(primerLetra)) {
+      result.push(nombres[index1]);
+    }
+  }
+
+  console.log(`Los nombres que comienzan con vocal son: ${result.join(", ")}`);
+}
+
+main();
